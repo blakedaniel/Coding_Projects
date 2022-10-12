967a0322ea4f73fbf24f7bb2b7648e9f08db9373
blakedaniel <blake.vanfleteren@gmail.com>
2022-10-12 11:01:58 -0400

    Revert "Update tic_tac_toe.py"
    This reverts commit 80e14cbd626d3fb698f87d94dfc6d28c82c04ade.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,12 +7,12 @@
 # [] combine winning functions into one main function
 
 
+
 # initialize
 import re
 
-n = tuple(range(0, 3))
-board_values = set([(x, y) for x in n for y in n])
-
+board_values = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1),
+                (2, 2)]
 player_x_moves = []
 player_o_moves = []
 player_moves = []
